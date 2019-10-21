f579a596af6d1e3564f02489737320c1e9572469
Kimjeongyoon <mmh2018@naver.com>
2019-10-21 19:12:28 +0900

    Drill08-1 success
    Drill08-1 https://youtu.be/gYrAvx2GC8o
    2018180012 김정윤 커밋횟수 1회

---
+++ Drill-08-2/pause_two.py
@@ -0,0 +1,46 @@
+from pico2d import *
+
+import game_framework
+import main_state
+
+class pause_start:
+    def __init__(self):
+        self.image = load_image('pause.png')
+
+    def update(self):
+        delay(0.115)
+        pass
+
+    def draw(self):
+        self.image.draw(400, 300, 300, 300)
+
+
+def enter():
+    global pause
+    pause = pause_start()
+
+
+def exit():
+    global pause
+    del (pause)
+
+
+def update():
+    global pause
+    pause.update()
+
+
+def draw():
+    global pause
+    clear_canvas()
+    main_state.draw()
+    pause.update()
+    pause.draw()
+    update_canvas()
+
+
+def handle_events():
+    events = get_events()
+    for event in events:
+        if event.type == SDL_KEYDOWN and event.key == SDLK_p:
+            game_framework.pop_state()
