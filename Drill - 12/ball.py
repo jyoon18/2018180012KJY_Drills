213b984845356f34a48736aa58215e4bc5a7591b
Kimjeongyoon <mmh2018@naver.com>
2019-11-20 18:02:09 +0900

    중간커밋
    중간커밋

---
+++ Drill - 12/ball.py
@@ -0,0 +1,28 @@
+import random
+from pico2d import *
+import game_world
+import game_framework
+
+class Ball:
+    image = None
+
+    def __init__(self):
+        if Ball.image == None:
+            Ball.image = load_image('ball21x21.png')
+        self.x, self.y = random.randint(100, 1180), random.randint(100, 324)
+
+    def get_bb(self):
+        return self.x - 10, self.y - 10, self.x + 10, self.y + 10
+
+    def draw(self):
+        self.image.draw(self.x, self.y)
+        draw_rectangle(*self.get_bb())
+
+    def update(self):
+        pass
+
+    #fill here for def stop
+
+
+# fill here
+# class BigBall
---
+++ Drill - 12/main_state.py
@@ -0,0 +1,111 @@
+import random
+import json
+import os
+
+from pico2d import *
+import game_framework
+import game_world
+
+from boy import Boy
+from ground import Ground
+from zombie import Zombie
+from ball import Ball
+
+
+name = "MainState"
+
+boy = None
+zombie = None
+balls = None
+
+
+def collide(a, b):
+    # fill here
+    left_a, bottom_a, right_a, top_a = a.get_bb()
+    left_b, bottom_b, right_b, top_b = b.get_bb()
+
+    if left_a > right_b: return False
+    if right_a < left_b: return False
+    if top_a < bottom_b: return False
+    if bottom_a > top_b: return False
+
+    return True
+
+
+
+def get_boy():
+    return boy
+
+
+def enter():
+    global boy
+    boy = Boy()
+    game_world.add_object(boy, 1)
+
+    global zombie
+    zombie = Zombie()
+    game_world.add_object(zombie, 1)
+
+    ground = Ground()
+    game_world.add_object(ground, 0)
+
+    global balls
+    balls = [Ball() for i in range(10)]
+    game_world.add_objects(balls, 1)
+
+
+def exit():
+    game_world.clear()
+
+def pause():
+    pass
+
+
+def resume():
+    pass
+
+
+def handle_events():
+    events = get_events()
+    for event in events:
+        if event.type == SDL_QUIT:
+            game_framework.quit()
+        elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
+                game_framework.quit()
+        else:
+            boy.handle_event(event)
+
+
+def update():
+    global boy, zombie, ball
+    for game_object in game_world.all_objects():
+        game_object.update()
+
+    if collide(boy, zombie):
+        print("collide check")
+
+    for ball in balls:
+        if collide(boy, ball):
+            balls.remove(ball)
+            game_world.remove_object(ball)
+            boy.boy_hp += 100
+
+    for ball in balls:
+        if collide(zombie, ball):
+            balls.remove(ball)
+            game_world.remove_object(ball)
+            zombie.zombie_hp += 100
+
+
+
+def draw():
+    clear_canvas()
+    for game_object in game_world.all_objects():
+        game_object.draw()
+    update_canvas()
+
+
+
+
+
+
